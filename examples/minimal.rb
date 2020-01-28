c996358d44eec852b937a803c57153923be20750
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-01-29 10:12:08 +1300

    More examples.

--- examples/minimal.rb
+++ examples/minimal.rb
@@ -1,5 +1,4 @@
 
-
 class Threaded
 	def initialize(&block)
 		@channel = Channel.new
@@ -12,34 +11,83 @@ class Threaded
 				finished(error)
 			else
 				finished
-			ensure
-				@channel.close
 			end
 		end
 	end
 	
+	attr :channel
+	
+	def close
+		self.terminate!
+		self.wait
+	ensure
+		@channel.close
+	end
+	
+	def interrupt!
+		@thread.raise(Interrupt)
+	end
+	
+	def terminate!
+		@thread.raise(Terminate)
+	end
+	
 	def wait
+		if @waiter
+			@waiter.join
+			@waiter = nil
+		end
 		
+		return @status
 	end
 	
 	protected
 	
 	def finished(error = nil)
 		@status = Status.new(error)
-		@channel.close_write
+		@channel.out.close
 	end
 end
 
 class Forked
 	def initialize(&block)
 		@channel = Channel.new
+		@status = nil
 		
-		@pid = Process.fork(&block)
+		@pid = Process.fork do
+			Signal.trap(:INT) {raise Interrupt}
+			Signal.trap(:INT) {raise Terminate}
+			
+			@channel.in.close
+			
+			yield
+		end
 		
-		@channel.close_write
+		@channel.out.close
 	end
 	
-	protected
+	attr :channel
 	
+	def close
+		self.terminate!
+		self.wait
+	ensure
+		@channel.close
+	end
 	
+	def interrupt!
+		Process.kill(:INT, @pid)
+	end
+	
+	def terminate!
+		Process.kill(:TERM, @pid)
+	end
+	
+	def wait
+		unless @status
+			pid, @status = ::Process.wait(@pid)
+		end
+		
+		return @status
+	end
 end
