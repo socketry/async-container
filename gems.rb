11e391e160a3250d4bd353426a9e8f4781a7c340
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-07-09 15:00:35 +1200

    Modernize gem.

--- gems.rb
+++ gems.rb
@@ -4,3 +4,10 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in utopia.gemspec
 gemspec
+
+group :maintenance, optional: true do
+	gem "bake-bundler"
+	gem "bake-modernize"
+	
+	gem "utopia-project"
+end
