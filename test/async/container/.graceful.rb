3656827ac2093490c730afd4f1eb79e23bff7184
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-03-26 15:58:15 +1300

    Avoid writing to stderr. This seems a bit more reliable.

--- test/async/container/.graceful.rb
+++ test/async/container/.graceful.rb
@@ -6,6 +6,8 @@
 
 require_relative '../../../lib/async/container/controller'
 
+$stdout.sync = true
+
 class Graceful < Async::Container::Controller
 	def setup(container)
 		container.run(name: "graceful", count: 1, restart: true) do |instance|
@@ -14,22 +16,25 @@ class Graceful < Async::Container::Controller
 			
 			original_action = Signal.trap(:INT) do
 				$stdout.puts "Graceful shutdown...", clock.total
-				$stdout.flush
 				
 				Signal.trap(:INT, original_action)
 			end
 			
 			$stdout.puts "Ready...", clock.total
-			$stdout.flush
 			
 			sleep
 		ensure
 			$stdout.puts "Exiting...", clock.total
-			$stdout.flush
 		end
 	end
 end
 
 controller = Graceful.new(graceful_stop: 1)
 
-controller.run
+begin
+	controller.run
+rescue Async::Container::Terminate
+	$stdout.puts "Terminated..."
+rescue Interrupt
+	$stdout.puts "Interrupted..."
+end
