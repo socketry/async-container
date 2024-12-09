9999fe11da4ca5e0970bafbf8a13f44f4094dd8e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-12-09 19:44:37 +1300

    Update copyrights.

--- lib/async/container/notify/pipe.rb
+++ lib/async/container/notify/pipe.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2020-2022, by Samuel Williams.
+# Copyright, 2020-2024, by Samuel Williams.
 # Copyright, 2020, by Juan Antonio Martín Lucas.
 
 require_relative "client"
