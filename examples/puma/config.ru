d625155af5d46b5fc0c0ee1f02639691126b77a6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-01-10 17:55:17 +1300

    Modernize code.

--- examples/puma/config.ru
+++ examples/puma/config.ru
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 run do |env|
 	[200, {"content-type" => "text/plain"}, ["Hello World #{Time.now}"]]
 end
--- examples/puma/puma.rb
+++ examples/puma/puma.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024-2025, by Samuel Williams.
+
 on_booted do
 	require "async/container/notify"
 	
