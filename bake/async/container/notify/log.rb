8b535a9b852f379a114c46ea527cb4bf6ac28574
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-08-04 10:07:33 +1200

    Documentation.

--- bake/async/container/notify/log.rb
+++ bake/async/container/notify/log.rb
@@ -6,6 +6,7 @@ def initialize(...)
 end
 
 # Check if the log file exists and the service is ready.
+# @parameter path [String] The path to the notification log file, uses the `NOTIFY_LOG` environment variable if not provided.
 def ready?(path: Async::Container::Notify::Log.path)
 	if File.exist?(path)
 		File.foreach(path) do |line|
