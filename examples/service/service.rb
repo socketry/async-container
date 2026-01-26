bd5ac5865772968853d24a267a6f77c9ca9e93ff
Samuel Williams <samuel.williams@shopify.com>
2026-01-25 20:58:23 -0800

    Modernize code.

--- examples/service/service.rb
+++ examples/service/service.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2024-2025, by Samuel Williams.
+# Copyright, 2026, by Samuel Williams.
 
 require "async"
 require "async/service/managed_service"
