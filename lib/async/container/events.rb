8001225268d07d3b0bff70d21be2779f16e7e6b3
Samuel Williams <samuel.williams@shopify.com>
2026-06-28 11:56:01 +1200

    Avoid exceptions when waking events.
    Assisted-By: devx/3236e566-7538-432e-a30a-2bdf37265ed4

--- lib/async/container/events.rb
+++ lib/async/container/events.rb
@@ -25,34 +25,21 @@ module Async
 			def <<(event)
 				@queue << event
 				
-				begin
-					@output.write_nonblock(".")
-				rescue ::IO::WaitWritable
-					# The pipe is already full, so any select waiter is already awake:
-				end
+				# If the pipe is full, any select waiter is already awake:
+				@output.write_nonblock(".", exception: false)
 				
 				return self
 			end
 			
 			# Remove and return the next queued event.
-			# @parameter non_block [Boolean] Whether to raise if no event is ready.
-			# @parameter timeout [Numeric | Nil] The maximum time to wait for an event.
-			def pop(non_block = false, timeout: nil)
-				if timeout
-					event = @queue.pop(non_block, timeout: timeout)
-				else
-					event = @queue.pop(non_block)
-				end
+			def pop(...)
+				event = @queue.pop(...)
 				
 				return unless event
 				
-				@input.read_nonblock(1)
+				@input.read_nonblock(1, exception: false)
 				
 				return event
-			rescue ::IO::WaitReadable
-				return event if event
-				
-				raise
 			end
 		end
 	end
