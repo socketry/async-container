187b0556c37f9d98c03dacd568f89c3d3d56cc9f
Samuel Williams <samuel.williams@shopify.com>
2026-06-28 21:26:16 +1200

    Move version constant.

--- lib/async/container.rb
+++ lib/async/container.rb
@@ -3,12 +3,4 @@
 # Released under the MIT License.
 # Copyright, 2017-2025, by Samuel Williams.
 
-require_relative "container/events"
 require_relative "container/controller"
-
-# @namespace
-module Async
-	# @namespace
-	module Container
-	end
-end
